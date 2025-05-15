b13307a9dd18cc246e84a221e0bc642fb4f3b0e0
vento <vento11b@gmail.com>
2025-05-15 04:10:01 +0200

    3

--- sw.js
+++ sw.js
@@ -3,12 +3,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('hola-mundo-cache').then(cache => {
       return cache.addAll([
-        'Spotifire/',
-        'Spotifire/index.html',
-        'Spotifire/style.css',
-        'Spotifire/sw.js',
-        'Spotifire/manifest.json',
-        'Spotifire/logo.png'
+        '/Spotifire/',
+        '/Spotifire/index.html',
+        '/Spotifire/style.css',
+        '/Spotifire/sw.js',
+        '/Spotifire/manifest.json',
+        '/Spotifire/logo.png'
       ]);
     })
   );
